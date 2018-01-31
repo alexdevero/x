338b6497ea74b930443dca8767dcf8a4bd4ba488
Alex Devero <deveroalex@gmail.com>
2018-01-31 09:43:53 +0100

    Add source files

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// import App from './app/components/App'
+
+const App = () => (
+  <div>
+    <h1>Death clock</h1>
+  </div>
+)
+
+export default App
---
+++ src/renderer/index.js
@@ -0,0 +1,8 @@
+// Initial welcome page. Delete the following line to remove it.
+import React from 'react'
+import { render } from 'react-dom'
+
+import App from '../app/App'
+
+// Now we can render our application into it
+render(<App />, document.getElementById('app'))
